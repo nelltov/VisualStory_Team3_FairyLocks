ca16d4be65374012ca89218b2827c246b3170971
Nellie Tonev <nellie.tonev@gmail.com>
2024-11-21 20:59:44 -0500

    set up news UI

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -50,46 +50,46 @@ public class GameManager : Singleton<GameManager>
         News.Instance.newspop();
         while (!managerCallsPlayed[1]) yield return null;
 
+        Debug.Log("Finished the game!");
+
         // TODO: Set up the UI for editing sequence #2
         // TODO: Wait until the player finishes the second editing sequence
         // TODO: Show feedback #2 for a couple seconds
 
 
-        // Play call #3 and wait for the call to be done
-        PhoneVideo.Instance.x = 2;
-        PhoneVideo.Instance.Call();
-        while (!managerCallsPlayed[2]) yield return null;
+        //// Play call #3 and wait for the call to be done
+        //PhoneVideo.Instance.x = 2;
+        //PhoneVideo.Instance.Call();
+        //while (!managerCallsPlayed[2]) yield return null;
 
-        // TODO: Set up the UI for editing sequence #3
-        // TODO: Wait until the player finishes the third editing sequence
-        // TODO: Show feedback #3 for a couple seconds
-
-
-        // Play call #4 and wait for the call to be done
-        PhoneVideo.Instance.x = 3;
-        PhoneVideo.Instance.Call();
-        while (!managerCallsPlayed[3]) yield return null;
-
-        // Set up the UI for editing sequence #4
-        // Wait until the player clicks "publish" on editing video #4
-        FilterManager.Instance.x = 4;
-        FilterManager.Instance.flagValue = 2;
-        FilterManager.Instance.feedback = false;
-        FilterManager.Instance.InitializeFilterUI();
-        while (!activismVidPublished) yield return null;
-
-        // TODO: Show feedback #4 for a couple seconds
+        //// TODO: Set up the UI for editing sequence #3
+        //// TODO: Wait until the player finishes the third editing sequence
+        //// TODO: Show feedback #3 for a couple seconds
 
 
-        // Play call #5 and wait for the call to be done
-        PhoneVideo.Instance.x = 4;
-        PhoneVideo.Instance.Call();
-        while (!managerCallsPlayed[4]) yield return null;
+        //// Play call #4 and wait for the call to be done
+        //PhoneVideo.Instance.x = 3;
+        //PhoneVideo.Instance.Call();
+        //while (!managerCallsPlayed[3]) yield return null;
 
-        // TODO: Set up the UI for editing sequence #5 (two choices for player)
-        // TODO: Wait until player clicks either "publish" or "delete" on editing video #5
-        // TODO: Play either ending based on whether the player clicked "publish" or "delete"
+        //// Set up the UI for editing sequence #4
+        //// Wait until the player clicks "publish" on editing video #4
+        //FilterManager.Instance.x = 4;
+        //FilterManager.Instance.flagValue = 2;
+        //FilterManager.Instance.feedback = false;
+        //FilterManager.Instance.InitializeFilterUI();
+        //while (!activismVidPublished) yield return null;
 
-        Debug.Log("Finished the game!");
+        //// TODO: Show feedback #4 for a couple seconds
+
+
+        //// Play call #5 and wait for the call to be done
+        //PhoneVideo.Instance.x = 4;
+        //PhoneVideo.Instance.Call();
+        //while (!managerCallsPlayed[4]) yield return null;
+
+        //// TODO: Set up the UI for editing sequence #5 (two choices for player)
+        //// TODO: Wait until player clicks either "publish" or "delete" on editing video #5
+        //// TODO: Play either ending based on whether the player clicked "publish" or "delete"
     }
 }
